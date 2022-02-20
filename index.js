c2fe2acba38b95a6478684cbdbfb4790f1f93a51
ayub-29 <ayubhagiali@outlook.com>
2022-02-19 23:05:19 -0500

    test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const dao     = require('./dao.js');
+const cors = require('cors')
 const app  = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
